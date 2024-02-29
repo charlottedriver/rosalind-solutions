c618ffa160a29ef08d97b44388539c437db01dee
charlottedriver <144388056+charlottedriver@users.noreply.github.com>
2024-02-29 23:48:20 +0000

    Function Outline

--- rosalind_GC.py
+++ rosalind_GC.py
@@ -6,3 +6,8 @@
 #
 # Return: The ID of the string having the highest GC-content, followed
 # by the GC-content of that string. 
+
+def gc_content(fasta_file):
+    id = ''
+    gc = 0
+    return id, gc
