22e050a110dc48da37998d0463955d977e20a887
charlottedriver <144388056+charlottedriver@users.noreply.github.com>
2024-02-29 23:46:44 +0000

    Problem Description

---
+++ rosalind_GC.py
@@ -0,0 +1,8 @@
+# Computing GC Content
+#
+#
+# Given: At most 10 DNA strings in Fasta format (of length at most 1
+# kbp each).
+#
+# Return: The ID of the string having the highest GC-content, followed
+# by the GC-content of that string. 
