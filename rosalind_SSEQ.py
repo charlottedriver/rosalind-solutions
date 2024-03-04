9fc52783d408388ec4b818fdcc30202a715e80ab
charlottedriver <144388056+charlottedriver@users.noreply.github.com>
2024-03-04 00:19:53 +0000

    Problem Outline

---
+++ rosalind_SSEQ.py
@@ -0,0 +1,13 @@
+# Finding a Spliced Motif
+#
+#
+# Given: Two DNA strings s and t (each at most 1 kbp) in FASTA format
+#
+# Return: One collection of indices of s in which the symbols of t
+# appear as a subsequences of s. If multiple solutions exisit, you
+# may return any one.
+
+def find_spliced_motif(dna_string, subsequence):
+    instances = []
+
+    return instances
