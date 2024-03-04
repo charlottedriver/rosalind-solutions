ded81d652f8ef630933fa04169258be65cbda5d0
charlottedriver <144388056+charlottedriver@users.noreply.github.com>
2024-03-04 18:43:58 +0000

    Update to Solution

--- rosalind_SSEQ.py
+++ rosalind_SSEQ.py
@@ -8,15 +8,27 @@
 # may return any one.
 
 def find_spliced_motif(dna_string, subsequence):
+    
     instances = []
+    
+    dna_string = dna_string.strip().upper()
+    subsequence = subsequence.strip().upper()
+
+    for i in range(len(dna_string) - len(subsequence) + 1):
+        if dna_string[i:i+len(subsequence)] == subsequence:
+            instances.append(i+1)
 
     return instances
 
 from Bio import SeqIO
 from Bio.Seq import Seq 
 
-fasta_file = 'practice.txt'
+fasta_file = 'rosalind_sseq.txt'
 sequences = list(SeqIO.parse(fasta_file, 'fasta'))
 
 dna_seq = str(sequences[0].seq)
 substring_seq = str(sequences[1].seq)
+
+print(find_spliced_motif(dna_seq, substring_seq))
+
+#49
