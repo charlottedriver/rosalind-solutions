b97b6f026b6850b42c2ae5bb6e5ab972d84b4f08
charlottedriver <144388056+charlottedriver@users.noreply.github.com>
2024-03-04 00:28:19 +0000

    Addition of Fasta FIle Reading

--- rosalind_SSEQ.py
+++ rosalind_SSEQ.py
@@ -11,3 +11,12 @@ def find_spliced_motif(dna_string, subsequence):
     instances = []
 
     return instances
+
+from Bio import SeqIO
+from Bio.Seq import Seq 
+
+fasta_file = 'practice.txt'
+sequences = list(SeqIO.parse(fasta_file, 'fasta'))
+
+dna_seq = str(sequences[0].seq)
+substring_seq = str(sequences[1].seq)
